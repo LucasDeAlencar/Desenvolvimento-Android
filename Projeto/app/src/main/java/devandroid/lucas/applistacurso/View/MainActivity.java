11624392396565403119ec4535d0de7f3a246303
lucas <lucas130333@gmail.com>
2024-01-10 11:50:14 -0300

    Primeiro Projeto Android

---
+++ Projeto/app/src/main/java/devandroid/lucas/applistacurso/View/MainActivity.java
@@ -0,0 +1,104 @@
+package devandroid.lucas.applistacurso.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import devandroid.lucas.applistacurso.Model.Pessoa;
+import devandroid.lucas.applistacurso.R;
+
+public class MainActivity extends AppCompatActivity {
+
+//    Pessoa pessoa;
+//    Pessoa outraPessoa;
+//
+//    String dadosPessoa;
+//    String dadosOutraPessoa;
+
+    EditText editPrimeiroNome;
+    EditText editSobreNome;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
+
+    /*Instancia o lanyout(TELA)*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Pessoa pessoa = new Pessoa(
+                "Nathan",
+                "Souza",
+                "Android",
+                "25 98795-9765");
+
+        /*Associação ao XML pro JAVA*/
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobreNome = findViewById(R.id.editSobreNome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        /*Para ocupar o campo(Serve para o EditText)*/
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobreNome.setText(pessoa.getSobreNome());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        /*Associar um comando ao click deste botão*/
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editPrimeiroNome.setText(null);
+                editSobreNome.setText(null);
+                editNomeCurso.setText(null);
+                editTelefoneContato.setText(null);
+
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*E parecido com o JOptionPane*/
+                Toast.makeText(MainActivity.this/*Está referenciando a propria classe */,
+                        "Volte Sempre",
+                        Toast.LENGTH_LONG/*Pede para demorar um tempo antes de execultar*/).show();
+
+                finish();/*Fecha a aplicação*/
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pessoa NovaPessoa = new Pessoa(
+                        editPrimeiroNome.getText().toString(),
+                        editSobreNome.getText().toString(),
+                        editNomeCurso.getText().toString(),
+                        editTelefoneContato.getText().toString()
+                );
+
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Foi criado com sucesso o usuario: \n\" " + NovaPessoa.getPrimeiroNome() + " " + NovaPessoa.getSobreNome() + " \" ",
+                            Toast.LENGTH_LONG
+                    ).show();
+            }
+        });
+
+    }
+}
